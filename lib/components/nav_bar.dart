4d0d0056db79c27ebf2408d2a1641eb152cb6f5a
parsashu <a.zshahidi91@gmail.com>
2024-08-17 16:29:10 +0330

    NavBar redesigned

--- lib/components/nav_bar.dart
+++ lib/components/nav_bar.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
 
-
 class NavBar extends StatefulWidget {
-
   final int selectedIndex;
   final ValueChanged<int> onTabChange;
 
@@ -18,59 +16,59 @@ class NavBar extends StatefulWidget {
 }
 
 class _NavBarState extends State<NavBar> {
-
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
-        child: GNav(
-          backgroundColor: Colors.white,
-          color: Colors.black,
-          activeColor: Colors.white,
-          tabBackgroundGradient: const LinearGradient(colors: [
-            Color.fromARGB(255, 90, 176, 222), 
-            Color.fromARGB(255, 186, 224, 244)
-            ]),
-          gap: 12,
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 27),
-          tabBorderRadius: 20,
-          iconSize:  25,
-          textStyle: const TextStyle(
-                color: Colors.white,
-                fontSize: 14,
-                fontWeight: FontWeight.w400,
-              ),   
-          onTabChange: (index) {
-            widget.onTabChange(index); // Call the callback function when tab changes
-          },
-          selectedIndex: widget.selectedIndex, // Pass the selectedIndex to GNav
-          tabs: const [
-            GButton(
-              icon: Icons.home_outlined,
-              text: 'Home', 
-
-            ),
-            GButton(
-              icon: Icons.chat_bubble_outline,
-              text: 'Chat',
-              
-            ),
-            GButton(
-              icon: Icons.person_outlined,
-              text: 'Profile',
-
-            ),
-            GButton(
-              icon: Icons.settings_outlined,
-              text: 'Settings',
-
-            ),
-          ],
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        const Divider(
+          height: 1,
+          thickness: 1,
         ),
-      ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 10),
+          child: GNav(
+            color: Colors.grey.shade600,
+            activeColor: Colors.black,
+            gap: 12,
+            padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 27),
+            tabBorderRadius: 40,
+            tabActiveBorder: Border.all(
+              color: Colors.grey.shade700, // Border color
+              width: 2, // Border width
+            ),
+            iconSize: 25,
+            textStyle: const TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.w400,
+            ),
+            onTabChange: (index) {
+              widget.onTabChange(
+                  index); // Call the callback function when tab changes
+            },
+            selectedIndex:
+                widget.selectedIndex, // Pass the selectedIndex to GNav
+            tabs: const [
+              GButton(
+                icon: Icons.home_outlined,
+                text: 'Home',
+              ),
+              GButton(
+                icon: Icons.chat_bubble_outline,
+                text: 'Chat',
+              ),
+              GButton(
+                icon: Icons.person_outlined,
+                text: 'Profile',
+              ),
+              GButton(
+                icon: Icons.settings_outlined,
+                text: 'Settings',
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
