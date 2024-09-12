ba435839dff5e00e3a7f9fe8cde85d76a0330a7e
parsashu <a.zshahidi91@gmail.com>
2024-09-12 10:00:32 +0330

    a

--- lib/components/containers/about_section.dart
+++ lib/components/containers/about_section.dart
@@ -1,5 +1,4 @@
 import 'package:fluentui_system_icons/fluentui_system_icons.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax_plus/iconsax_plus.dart';
 
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:improwave/components/bars/nav_bar.dart';
+import 'package:improwave/pages/main/home_page.dart';
+import 'package:improwave/pages/main/search_page.dart';
+import 'package:improwave/pages/profile/Ath_profile_page.dart';
+import 'package:improwave/pages/profile/tra_profile_page.dart';
+import 'package:improwave/utils/is_trainer_provider.dart';
+import 'package:provider/provider.dart';
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  // Default page
+  int _selectedIndex = 1;
+
+  void _onTabChange(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isTrainer = Provider.of<IsTrainerProvider>(context).isTrainer;
+
+    // Pages
+    final List<Widget> pages = [
+      const HomePage(),
+      const SearchPage(),
+      isTrainer ? const TraProfilePage() : const AthProfilePage()
+    ];
+
+    return Scaffold(
+      body: pages[_selectedIndex],
+      bottomNavigationBar: NavBar(
+        selectedIndex: _selectedIndex,
+        onTabChange: _onTabChange,
+      ),
+    );
+  }
+}
